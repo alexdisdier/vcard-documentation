caaa1bd900bfa1a8fa69675917f6eb3a377c1f09
Alexandre Disdier <alexandredisdier@Alexandres-MacBook-Pro.local>
2018-10-16 22:59:00 -0500

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/* main.js */
+
+/*--------------------------------------------------------------
+ toggleDropdown Mobile and <a> active
+--------------------------------------------------------------*/
+var toggleContent = document.getElementById('js-dropdown-content');
+var toggleLinks = document.getElementsByClassName("nav-link");
+var menuIcon = document.getElementById('js-menu-icon'); // The burger button triggers the toggle open
+
+function toggleDropdown() {
+  if (toggleContent.style.display === "block") {
+    toggleContent.style.display = "none";
+    menuIcon.classList.remove("is-active"); // console.log("toggleOpen");
+  } else {
+    toggleContent.style.display = "block";
+    menuIcon.classList.add("is-active"); // console.log("toggleClose");
+  }
+} // If you click on one of the <a> element.
+
+
+for (var i = 0; i < toggleLinks.length; i++) {
+  toggleLinks[i].addEventListener("click", function () {
+    // console.log("toggleClose");
+    toggleContent.style.display = "none";
+    menuIcon.classList.remove("is-active"); // toggleLinks[i].classList.add("js-active");
+  });
+} // Add the class Active when you scroll to the section or if the <a> is clicked
